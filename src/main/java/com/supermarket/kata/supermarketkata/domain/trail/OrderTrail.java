dfeda4975ea089986fc1b6b87638048bf3eb5e39
Mahdi <mahdi.maiche@zsoft-consulting.com>
2019-03-08 17:43:43 +0100

    Added trail entities

---
+++ src/main/java/com/supermarket/kata/supermarketkata/domain/trail/OrderTrail.java
@@ -0,0 +1,11 @@
+package com.supermarket.kata.supermarketkata.domain.trail;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderTrail {
+
+    private String orderId;
+    private String userPricingStrategyId;
+}
---
+++ src/main/java/com/supermarket/kata/supermarketkata/domain/trail/ProductTrail.java
@@ -0,0 +1,13 @@
+package com.supermarket.kata.supermarketkata.domain.trail;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ProductTrail {
+
+    private String productId;
+    private String productUnitPrice;
+    private String usedPricingStrategyId;
+
+}
