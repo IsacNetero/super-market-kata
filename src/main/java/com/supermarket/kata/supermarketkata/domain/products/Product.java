73a74fc97921af08c305acf6a64537b9831a9321
Mahdi <mahdi.maiche@zsoft-consulting.com>
2019-03-08 13:30:52 +0100

    Added constructor and billing strategy mutator for Product class

--- src/main/java/com/supermarket/kata/supermarketkata/domain/products/Product.java
+++ src/main/java/com/supermarket/kata/supermarketkata/domain/products/Product.java
@@ -3,7 +3,9 @@ package com.supermarket.kata.supermarketkata.domain.products;
 import com.supermarket.kata.supermarketkata.domain.billing.ProductBillingStrategy;
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Getter
 public class Product {
@@ -13,5 +15,18 @@ public class Product {
     private float unitPrice;
     private ProductBillingType billingType;
     private List<ProductBillingStrategy> billingStrategies;
+
+    public Product(String name, float unitPrice, ProductBillingType billingType){
+
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.unitPrice = unitPrice;
+        this.billingType = billingType;
+        this.billingStrategies = new ArrayList<>();
+
+    }
+    public void addBillingStrategy(ProductBillingStrategy billingStrategy){
+        this.billingStrategies.add(billingStrategy);
+    }
 
 }
