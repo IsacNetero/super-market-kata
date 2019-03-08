7067fc5f9ba22e2de50dc613c5dddb6228a253b6
Mahdi <mahdi.maiche@zsoft-consulting.com>
2019-03-08 16:04:15 +0100

    Added constructor and mutators to Order class

--- src/main/java/com/supermarket/kata/supermarketkata/domain/orders/Order.java
+++ src/main/java/com/supermarket/kata/supermarketkata/domain/orders/Order.java
@@ -1,8 +1,10 @@
 package com.supermarket.kata.supermarketkata.domain.orders;
 
 import com.supermarket.kata.supermarketkata.domain.billing.OrderPricingStrategy;
+import com.supermarket.kata.supermarketkata.domain.products.Product;
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -10,5 +12,21 @@ public class Order {
 
     private String id;
     private List<OrderItem> orderedItems;
-    private List<OrderPricingStrategy> billingStrategies;
+    private List<OrderPricingStrategy> pricingStrategies;
+
+
+    public Order(){
+
+        this.id = java.util.UUID.randomUUID().toString();
+        this.orderedItems = new ArrayList<>();
+        this.pricingStrategies = new ArrayList<>();
+    }
+
+    public void addItem(Product product, float quantity) throws Exception {
+        this.orderedItems.add(new OrderItem(product, quantity));
+    }
+
+    public void addPricingStrategy(OrderPricingStrategy pricingStrategy) {
+        this.pricingStrategies.add(pricingStrategy);
+    }
 }
