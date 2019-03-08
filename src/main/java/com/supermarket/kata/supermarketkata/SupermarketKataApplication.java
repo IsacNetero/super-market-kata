8964d01517ca1b4ad4060567cd0fac77a1bb299c
Mahdi <mahdi.maiche@zsoft-consulting.com>
2019-03-08 13:14:32 +0100

    Bootstraped project

---
+++ src/main/java/com/supermarket/kata/supermarketkata/SupermarketKataApplication.java
@@ -0,0 +1,13 @@
+package com.supermarket.kata.supermarketkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketKataApplication.class, args);
+	}
+
+}
